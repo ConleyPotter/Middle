50a28ac1d47192cbd1448deec0810f5f41b02cf3
Conley Potter <conley.potter@gmail.com>
2019-05-31 08:44:08 -0500

    Seed database for production

--- frontend/components/main/articles/article_editor.jsx
+++ frontend/components/main/articles/article_editor.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Redirect } from 'react-router'
-import EditorPopup from './editor_popup'
 
 class ArticleEdtor extends React.Component {
   constructor(props) {
@@ -8,7 +7,7 @@ class ArticleEdtor extends React.Component {
 
     this.state = {
       errors: [],
-      body: null,
+      body: null
     };
 
     this.contentEditableDiv = React.createRef();
@@ -18,9 +17,7 @@ class ArticleEdtor extends React.Component {
     this.focusOnEditable = this.focusOnEditable.bind(this);
   }
 
-  fireSelectionMonitor(event) {
-
-  }
+  fireSelectionMonitor(event) {}
 
   focusOnEditable() {
     this.contentEditableDiv.current.focus();
@@ -28,7 +25,7 @@ class ArticleEdtor extends React.Component {
 
   componentDidMount() {
     if (this.props.match.params.articleId) {
-      this.props.fetchArticle(this.props.match.params.articleId)
+      this.props.fetchArticle(this.props.match.params.articleId);
     } else {
       this.setState({
         displayedArticle: "new"
@@ -42,15 +39,12 @@ class ArticleEdtor extends React.Component {
       this.setState({
         displayedArticle: this.props.article
       });
-    } 
+    }
     if (document.getSelection.type) {
       const selected = document.getSelection.toString();
       document
         .getSelection()
-        .anchorNode.addEventListener(
-          "mouseup",
-          this.setState({})
-        );
+        .anchorNode.addEventListener("mouseup", this.setState({}));
       document.appendChild(<EditorPopup selected={selected} />);
     }
   }
@@ -70,10 +64,12 @@ class ArticleEdtor extends React.Component {
         author_id: this.props.current_user.id
         // how will I deal with the photo to attach? AWS
       });
+      // redirect to a user's profile page once its built
       // return <Redirect to={`articles/${this.props.article.id}`} />;
     } else {
-      const { author_id, id } = 
-        this.props.article[this.props.match.params.articleId]
+      const { author_id, id } = this.props.article[
+        this.props.match.params.articleId
+      ];
       this.props.updateArticle({
         body: this.state.body,
         title: this.state.title,
@@ -82,57 +78,98 @@ class ArticleEdtor extends React.Component {
         author_id: author_id,
         id: id
       });
-      // return <Redirect to={`articles/${this.props.article.id}`} />;
+      return <Redirect to={`articles/${this.props.article.id}`} />;
     }
   }
 
-  
-  render() {
-    document.addEventListener('mousedown', (event) => {
-      debugger;
-      if (typeof document.selection !="undefined" 
-               && document.selection.type == "Text") {
-        var text = document.selection.createRange().text;
-        event.target.appendChild(<EditorPopup />);
-      }
-    });
+  toolBarButtonClicked(e, styling) {
+    e.preventDefault();
+    document.execCommand(styling, false, null);
+    this.focusOnEditable();
+  }
 
-    const placeholderBody =
-      this.props.article[this.props.match.params.articleId]
+  render() {
+    const placeholderBody = this.props.article[
+      this.props.match.params.articleId
+    ]
       ? this.props.article[this.props.match.params.articleId].body
       : "Tell your story...";
 
-    const placeholderTitle =
-      this.props.article[this.props.match.params.articleId]
+    const placeholderTitle = this.props.article[
+      this.props.match.params.articleId
+    ]
       ? this.props.article[this.props.match.params.articleId].title
       : "Title";
-    
+
     // I'm thinking that if there is content to render beside a placeholder I
-    // should append it to the outer div instead of just rendering it plainly
+    // should append it to the outer div instead of just rendering it plainlylololoipppoooiiiooiikiikjui89ppp0poo9ioiiu776ytr4eeer
     return (
-      <div 
-        id="innermost-outer-container-div" 
+      <div
+        id="innermost-outer-container-div"
         className="article-editor-container-outer"
       >
-        <div
-          className="article-editor-container-title"
-          contentEditable
-          suppressContentEditableWarning
-          onBlur={event => this.update(event, "title")}
-        >
-          {placeholderTitle}
+        <div className="article-editor-container-inner">
+          <div
+            className="article-editor-container-title"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={event => this.update(event, "title")}
+          >
+            {placeholderTitle}
+          </div>
+          <div className="toolbar-container">
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "bold")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-bold" />
+            </button>
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "italic")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-italic" />
+            </button>
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "underline")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-underline" />
+            </button>
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "justifyLeft")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-align-left" />
+            </button>
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "justifyCenter")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-align-center" />
+            </button>
+            <button
+              onClick={e => this.toolBarButtonClicked(e, "justifyRight")}
+              className="document-editor-buttons"
+            >
+              <span className="fa fa-align-right" />
+            </button>
+          </div>
+          <div
+            className="article-editor-container-body"
+            contentEditable
+            suppressContentEditableWarning
+            ref={this.contentEditableDiv}
+            onBlur={event => this.update(event, "body")}
+          >
+            {placeholderBody}
+          </div>
+          <div className="submit-wrapper">
+            <button className="submit-button" onClick={this.submitEditor}>
+              Submit
+            </button>
+          </div>
         </div>
-        <div
-          className="article-editor-container-body"
-          contentEditable
-          suppressContentEditableWarning
-          ref={this.contentEditableDiv}
-          onBlur={event => this.update(event, "body")}
-        >
-          {placeholderBody}
-        </div>
-        <button onClick={this.submitEditor}>Submit</button>
-
       </div>
     );
   }
